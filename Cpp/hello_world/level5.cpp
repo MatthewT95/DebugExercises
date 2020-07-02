ba917db984ef50ee0341d60cf432e64de1fa0e85
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:06:28 -0400

    Added hello_world/level5

---
+++ Cpp/hello_world/level5.cpp
@@ -0,0 +1,10 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint: The include has the wrong sytax
+*/
+#include iostream
+int main()
+{
+	std::cout << "Hello world" << std::endl;
+}
