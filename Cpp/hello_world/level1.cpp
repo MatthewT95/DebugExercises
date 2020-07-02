f9a134248935ee83eb8cd859f6c690f08dd395eb
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:05:38 -0400

    Added hello_world/level1

---
+++ Cpp/hello_world/level1.cpp
@@ -0,0 +1,11 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint:There is a sytax error on line 10.
+*/
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Hello world" << endl
+}
