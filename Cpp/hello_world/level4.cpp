b05aabd997f465fae451073e126dfdc9bd29b5b3
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:06:17 -0400

    Added hello_world/level4

---
+++ Cpp/hello_world/level4.cpp
@@ -0,0 +1,10 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint: There is single missing character.
+*/
+include <iostream>
+int main()
+{
+	std::cout << "Hello world" << std::endl;
+}
