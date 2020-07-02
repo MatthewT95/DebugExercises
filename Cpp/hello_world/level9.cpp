764395dbb72fdcb05e36e0a584eaad2c992c6fd0
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:08:07 -0400

    Added hello_world/level9

---
+++ Cpp/hello_world/level9.cpp
@@ -0,0 +1,11 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint:Something is missing.
+*/
+#include <iostream>
+//What should be here
+{
+	using namespace std;
+	cout << "Hello world" << endl;
+}
