39cbaca89a728eed44c6499e5ffe767c655a9258
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:06:41 -0400

    Added hello_world/level6

---
+++ Cpp/hello_world/level6.cpp
@@ -0,0 +1,10 @@
+/*
+program name: hello_world
+purpose: This program should print the message 'The computer says "Hello wolrd"' to console.
+Hint: If you want literal quotes inside of a string; you must use a special character.
+*/
+#include <iostream>
+int main()
+{
+	std::cout << "The computer says "Hello wolrd"" << std::endl;
+}
