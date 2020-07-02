882272c178c559bd72b7ce38c00bc5614a77584a
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:06:52 -0400

    Added hello_world/level7

---
+++ Cpp/hello_world/level7.cpp
@@ -0,0 +1,11 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint:The entire program is commented out.
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Hello world" << endl;
+}
