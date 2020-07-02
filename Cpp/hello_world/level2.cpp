d84f2eb63a5596fce36b6a7db828779f6266ae5d
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:05:50 -0400

    Added hello_world/level2

---
+++ Cpp/hello_world/level2.cpp
@@ -0,0 +1,10 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint: Missing an include.
+*/
+int main()
+{
+	using namespace std;
+	cout << "Hello world" << endl;
+}
