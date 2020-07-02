cc256477d894c9d50fdf3cb5835f7fa9ecbdfe86
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:07:21 -0400

    Added hello_world/level8

---
+++ Cpp/hello_world/level8a.cpp
@@ -0,0 +1,10 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint:The main function block is missing something at the end.
+*/
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Hello world" << endl;
---
+++ Cpp/hello_world/level8b.cpp
@@ -0,0 +1,11 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint:There was likly a typing error with the main function.
+*/
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Hello world" << endl;
+}}
