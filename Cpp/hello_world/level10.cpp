dc3a262bdcdfab7a9a3feb3e2569c55fc82d172c
Matthew Taormina <matt.taormina@outlook.com>
2020-07-01 22:08:16 -0400

    Added hello_world/level10

---
+++ Cpp/hello_world/level10.cpp
@@ -0,0 +1,11 @@
+/*
+program name: hello_world
+purpose: This program should print the message "Hello world" to console.
+Hint: Main is returning the wrong datatype
+*/
+#include <iostream>
+bool main()
+{
+	using namespace std;
+	cout << "Hello world" << endl;
+}
